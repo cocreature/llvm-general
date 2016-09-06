c58c1c0dd2076d3b1f6011b5a6d994ac578e568a
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-09-06 18:40:33 +0200

    Remove most of the FFI in Target.hs

--- llvm-general/src/LLVM/General/Internal/Module.hs
+++ llvm-general/src/LLVM/General/Internal/Module.hs
@@ -7,7 +7,6 @@
 -- | This Haskell module is for/of functions for handling LLVM modules.
 module LLVM.General.Internal.Module where
 
-import Control.Monad
 import Control.Exception
 import Control.Monad.AnyCont
 import Control.Monad.Error.Class
@@ -15,22 +14,15 @@ import Control.Monad.Trans.Except
 import Control.Monad.Trans
 
 import Foreign.Ptr
-import Foreign.C
-import Data.IORef
-import qualified Data.ByteString as BS
 
-import qualified LLVM.General.Internal.FFI.Bitcode as FFI
 import qualified LLVM.General.Internal.FFI.LLVMCTypes as FFI
 import qualified LLVM.General.Internal.FFI.MemoryBuffer as FFI
 import qualified LLVM.General.Internal.FFI.Module as FFI
-import qualified LLVM.General.Internal.FFI.RawOStream as FFI
-import qualified LLVM.General.Internal.FFI.Target as FFI
 
 import LLVM.General.Internal.Coding
 import LLVM.General.Internal.Context
 import LLVM.General.Internal.Inject
 import qualified LLVM.General.Internal.MemoryBuffer as MB
-import LLVM.General.Internal.RawOStream
 import LLVM.General.Internal.String
 import LLVM.General.Internal.Target
 
@@ -63,36 +55,6 @@ withModuleFromLLVMAssembly (Context c) s f = flip runAnyContT return $ do
   mb <- llvmAssemblyMemoryBuffer s
   m <- anyContToM $ bracket undefined FFI.disposeModule
   liftIO $ f (Module m)
-
-targetMachineEmit :: FFI.CodeGenFileType -> TargetMachine -> Module -> Ptr FFI.RawPWriteStream -> ExceptT String IO ()
-targetMachineEmit fileType (TargetMachine tm) (Module m) os = flip runAnyContT return $ do
-  msgPtr <- alloca
-  r <- decodeM =<< (liftIO $ FFI.targetMachineEmit tm m fileType msgPtr os)
-  when r $ throwError =<< decodeM msgPtr
-
-emitToFile :: FFI.CodeGenFileType -> TargetMachine -> File -> Module -> ExceptT String IO ()
-emitToFile fileType tm (File path) m = flip runAnyContT return $ do
-  withFileRawPWriteStream path False False $ targetMachineEmit fileType tm m
-
-emitToByteString :: FFI.CodeGenFileType -> TargetMachine -> Module -> ExceptT String IO BS.ByteString
-emitToByteString fileType tm m = flip runAnyContT return $ do
-  withBufferRawPWriteStream $ targetMachineEmit fileType tm m
-
--- | write target-specific assembly directly into a file
-writeTargetAssemblyToFile :: TargetMachine -> File -> Module -> ExceptT String IO ()
-writeTargetAssemblyToFile = emitToFile FFI.codeGenFileTypeAssembly
-
--- | produce target-specific assembly as a 'String'
-moduleTargetAssembly :: TargetMachine -> Module -> ExceptT String IO String
-moduleTargetAssembly tm m = decodeM . UTF8ByteString =<< emitToByteString FFI.codeGenFileTypeAssembly tm m
-
--- | produce target-specific object code as a 'ByteString'
-moduleObject :: TargetMachine -> Module -> ExceptT String IO BS.ByteString
-moduleObject = emitToByteString FFI.codeGenFileTypeObject
-
--- | write target-specific object code directly into a file
-writeObjectToFile :: TargetMachine -> File -> Module -> ExceptT String IO ()
-writeObjectToFile = emitToFile FFI.codeGenFileTypeObject
 
 getTargetTriple :: Ptr FFI.Module -> IO (Maybe String)
 getTargetTriple m = do
